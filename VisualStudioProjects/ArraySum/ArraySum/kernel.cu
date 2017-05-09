3ae3adbd8808b39946a2b51bfd85b6709f3055a8
Dalton <dnickers@unca.edu>
2017-05-08 22:07:01 -0400

    Finished and working.

--- VisualStudioProjects/ArraySum/ArraySum/kernel.cu
+++ VisualStudioProjects/ArraySum/ArraySum/kernel.cu
@@ -1,27 +1,31 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-cudaError_t sumArray(int *arr, int size, int *out, int threadsPerBlock);
+cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlock);
 
-__global__ void sumKernel(int *arr, int size)
+__global__ void sumKernel(long long *arr, int size)
 {
 	int i = threadIdx.x + blockDim.x*blockIdx.x;
 	if (i < size) {
-		arr[i] += arr[i+size];
+		//printf("Adding arr[%d]=%d and arr[%d]=%d\n", i, arr[i], i + size, arr[i + size]);
+		arr[i] += arr[i + size];
+		arr[i + size] = 0;
 	}
 }
 
 int main()
 {
-	const int arraySize = 20;
-	int a[arraySize];// = { 1, 2, 3, 4, 5,15 };
-	int sum = 0;
+	const int arraySize = 3000;
+	long long a[arraySize];// = { 1, 2, 3, 4, 5,15 };
+	long long sum = 0;
 	for (int i = 0; i < arraySize; i++) {
-		a[i] = i;
+		a[i] = rand();
 		sum += a[i];
+		//printf("i:%d\tsum so far:%d\n", i, sum);
 	}
-	printf("Actual sum: %d\n", sum);
+	printf("Actual sum: %lld\n", sum);
 	// Add vectors in parallel.
 	cudaError_t cudaStatus = sumArray(a, arraySize, &sum, 128);
 	if (cudaStatus != cudaSuccess) {
@@ -41,9 +45,8 @@ int main()
 }
 
 
-cudaError_t sumArray(int *arr, int size, int *out, int threadsPerBlock) {
+cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlock) {
 
-	printf("Running array sum with %d threads per block.\n", threadsPerBlock);
 	cudaError_t cudaStatus;
 	// Choose which GPU to run on, change this on a multi-GPU system.
 	cudaStatus = cudaSetDevice(0);
@@ -51,24 +54,33 @@ cudaError_t sumArray(int *arr, int size, int *out, int threadsPerBlock) {
 		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
 	}
 	else {
-		int adjustedSize = size;
-		if (adjustedSize % 32 != 0) {
-			adjustedSize += 32 - (size % 32);
-		}
 		//determine number of thread blocks required
 		int numThreadBlocks;
 		//create array for sum reduction on gpu
-		int *gpuArray;
-		cudaMalloc((void **)&gpuArray, adjustedSize * sizeof(int));
-		//initialize array to 0 on gpu
-		cudaMemset((void*)gpuArray, 0, adjustedSize);
+		long long *gpuArray;
+		//adjust size so it is even
+		int adjustedSize = size + size % 2;
+		cudaMalloc((void **)&gpuArray, adjustedSize * sizeof(long long));
+		cudaStatus = cudaDeviceSynchronize();
+		cudaMemset((void*)gpuArray, 0, adjustedSize * sizeof(long long));
+		cudaStatus = cudaDeviceSynchronize();
 		//copy input data to gpu
-		cudaMemcpy(gpuArray, arr, size, cudaMemcpyHostToDevice);
+		cudaMemcpy(gpuArray, arr, size * sizeof(long long), cudaMemcpyHostToDevice);
+		cudaStatus = cudaDeviceSynchronize();
 		//keep reducing the problem size by two
 		while (adjustedSize > 1) {
 			adjustedSize /= 2;
+			if (adjustedSize % 2 != 0 && adjustedSize>1) {
+				adjustedSize++;
+			}
+			if (adjustedSize < threadsPerBlock) {
+				threadsPerBlock = adjustedSize;
+				if (threadsPerBlock % 32 != 0) {
+					threadsPerBlock += 32 - threadsPerBlock % 32;
+				}
+			}
 			numThreadBlocks = (adjustedSize + threadsPerBlock - 1) / threadsPerBlock;
-			printf("Adjusted size:%d\tThreadBlocks:%d\n", adjustedSize, numThreadBlocks);
+			//printf("Adjusted size:%d\tThreadBlocks:%d\tThreadsPerBlock:%d\n", adjustedSize, numThreadBlocks,threadsPerBlock);
 			// Launch a kernel on the GPU with one thread for each pair of elements.
 			sumKernel << <numThreadBlocks, threadsPerBlock >> > (gpuArray, adjustedSize);
 
@@ -86,10 +98,10 @@ cudaError_t sumArray(int *arr, int size, int *out, int threadsPerBlock) {
 					fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
 					break;
 				}
-				cudaMemcpy(out, gpuArray, sizeof(int), cudaMemcpyDeviceToHost);
-				printf("PartialResult:%d\n", *out);
+				cudaMemcpy(out, gpuArray, sizeof(long long), cudaMemcpyDeviceToHost);
 			}
 		}
+		printf("PartialResult:%lld\n", *out);
 		cudaFree(gpuArray);
 	}
 	return cudaStatus;
