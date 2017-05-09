59ee91ad9dc609859e628b463617e59f05f2fcae
Dalton <dnickers@unca.edu>
2017-05-09 16:27:34 -0400

    Refactored array sum for importable generic sum.

--- VisualStudioProjects/ArraySum/ArraySum/GPU_Sum.cu
+++ VisualStudioProjects/ArraySum/ArraySum/GPU_Sum.cu
@@ -1,14 +1,10 @@
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <omp.h>
+#include "GPU_Sum.h"
 
 #define NUM_THREADS 8
-#define TIMING
-#define MIN_SIZE 100000000
+#define TIMINGNo
+#define MIN_SIZE 1000000
 #define SIZE_INCREMENT 250000
-#define MAX_SIZE 100000000
+#define MAX_SIZE 1000000
 #define SAMPLE_SIZE 1
 
 #ifdef TIMING
@@ -16,14 +12,27 @@ double avgCPUTime, avgGPUTime;
 double cpuStartTime, cpuEndTime;
 #endif // TIMING
 
-cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlock);
+template<typename Type>
+cudaError_t sumArray(Type *arr, int size, Type *out, int blockCount, int threadsPerBlock, bool arrayOnGPU);
 
-__global__ void sumKernel(long long *arr, int size)
+template<typename Type>
+__global__ void sumKernel(Type *arr, int size)
 {
-	int i = threadIdx.x + blockDim.x*blockIdx.x;
-	if (i < size) {
-		arr[i] += arr[i + size];
-		arr[i + size] = 0;
+	int stride = blockDim.x * gridDim.x;
+	int id = threadIdx.x + blockDim.x*blockIdx.x;
+	long long temp = arr[id];
+	for (int i = id + stride; i < size; i += stride) {
+		temp += arr[i];
+	}
+	arr[id] = temp;
+}
+
+template<typename Type>
+__global__ void binaryReduction(Type *arr, int size)
+{
+	int id = threadIdx.x + blockDim.x*blockIdx.x;
+	if (id < size) {
+		arr[id] += arr[id + size];
 	}
 }
 
@@ -31,7 +40,7 @@ int main()
 {
 	long long *a = (long long*)malloc(MAX_SIZE * sizeof(long long));
 	for (int i = 0; i < MAX_SIZE; i++) {
-		a[i] = rand();
+		a[i] = i;// rand();
 	}
 	int timesCorrect = 0, timesWrong = 0;
 	printf("Size\tAvgCPUTime\tAvgGPUTime\tSamples:%d\n", SAMPLE_SIZE);
@@ -40,11 +49,11 @@ int main()
 		avgCPUTime = 0;
 		avgGPUTime = 0;
 #endif // TIMING
+		long long sum = 0, cudaSum;
 #pragma omp parallel for num_threads(NUM_THREADS) \
 reduction(+:avgGPUTime,avgCPUTime,timesCorrect,timesWrong) \
 private(cpuStartTime,cpuEndTime)
 		for (int sample = 0; sample < SAMPLE_SIZE; sample++) {
-			long long sum = 0, cudaSum;
 #ifdef TIMING
 			cpuStartTime = omp_get_wtime();
 #endif // TIMING
@@ -57,7 +66,7 @@ private(cpuStartTime,cpuEndTime)
 			avgCPUTime += timeUsed;
 #endif // TIMING
 			// Add vectors in parallel.
-			cudaError_t cudaStatus = sumArray(a, arraySize, &cudaSum, 256);
+			cudaError_t cudaStatus = sumArray(a, arraySize, &cudaSum, 1, 256, false);
 			if (cudaStatus != cudaSuccess) {
 				fprintf(stderr, "CUDA Sum Array failed!");
 				return 1;
@@ -79,7 +88,7 @@ private(cpuStartTime,cpuEndTime)
 			}
 		}
 #ifdef TIMING
-		printf("%d\t%lf\t%lf\n", arraySize, avgCPUTime / SAMPLE_SIZE, avgGPUTime / SAMPLE_SIZE);
+		printf("%d\t%lf\t%lf\nCPU:%ld\tGPU:%ld\n", arraySize, avgCPUTime / SAMPLE_SIZE, avgGPUTime / SAMPLE_SIZE, sum, cudaSum);
 #endif // TIMING
 	}
 	printf("GPU Implementation was correct %d times and incorrect %d times.\n", timesCorrect, timesWrong);
@@ -87,13 +96,14 @@ private(cpuStartTime,cpuEndTime)
 }
 
 
-cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlock) {
+template<typename Type>
+cudaError_t sumArray(Type *arr, int size, Type *out, int blockCount, int blockSize, bool arrayOnGPU) {
 #ifdef TIMING
 	double totalGpuTimeUsed = 0;
 	float gpuTimeUsed;
 	cudaEvent_t startStep, endStep;
-	cudaEventCreateWithFlags(&startStep, cudaEventDefault);//cudaEventBlockingSync);
-	cudaEventCreateWithFlags(&endStep, cudaEventDefault);// cudaEventBlockingSync);
+	cudaEventCreateWithFlags(&startStep, cudaEventDefault);
+	cudaEventCreateWithFlags(&endStep, cudaEventDefault);
 #endif // TIMING
 	cudaError_t cudaStatus;
 	// Choose which GPU to run on, change this on a multi-GPU system.
@@ -107,46 +117,34 @@ cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlo
 		//adjust size so it is even
 		int adjustedSize = size + size % 2;
 		//determine number of thread blocks required
-		int numThreadBlocks = numThreadBlocks = (adjustedSize + threadsPerBlock - 1) / threadsPerBlock;
-		if (numThreadBlocks > 65535) {
-			long long part1, part2;
-			sumArray(arr, size / 2, &part1, threadsPerBlock);
-			sumArray(&arr[size / 2], size / 2, &part2, threadsPerBlock);
-			cudaStatus = cudaGetLastError();
-			if (cudaStatus == cudaSuccess) {
-				*out = part1 + part2;
-			}
-			return cudaStatus;
-		}
 		//create array for sum reduction on gpu
-		long long *gpuArray;
-		cudaMalloc((void **)&gpuArray, adjustedSize * sizeof(long long));
-		cudaStatus = cudaStreamSynchronize(myStream);
-		cudaMemsetAsync((void*)gpuArray, 0, adjustedSize * sizeof(long long),myStream);
-		cudaStatus = cudaStreamSynchronize(myStream);
-		//copy input data to gpu
-		cudaMemcpyAsync(gpuArray, arr, size * sizeof(long long), cudaMemcpyHostToDevice,myStream);
-		cudaStatus = cudaStreamSynchronize(myStream);
+		Type *gpuArray;
+		if (arrayOnGPU) {
+			gpuArray = arr;
+		}
+		else {
+			cudaMalloc((void **)&gpuArray, adjustedSize * sizeof(Type));
+			cudaStatus = cudaStreamSynchronize(myStream);
+			cudaMemsetAsync((void*)gpuArray, 0, adjustedSize * sizeof(Type), myStream);
+			cudaStatus = cudaStreamSynchronize(myStream);
+			//copy input data to gpu
+			cudaMemcpyAsync(gpuArray, arr, size * sizeof(Type), cudaMemcpyHostToDevice, myStream);
+			cudaStatus = cudaStreamSynchronize(myStream);
+		}
+		sumKernel << <blockCount, blockSize, 0, myStream >> > (gpuArray, adjustedSize);
+		adjustedSize = blockCount*blockSize;
 		//keep reducing the problem size by two
 		while (adjustedSize > 1) {
+			cudaStatus = cudaStreamSynchronize(myStream);
 #ifdef TIMING
-			cudaEventRecord(startStep,myStream);
-			cudaStreamSynchronize(myStream);
+			cudaEventRecord(startStep, myStream);
 #endif // TIMING
 			adjustedSize /= 2;
 			if (adjustedSize % 2 != 0 && adjustedSize > 1) {
 				adjustedSize++;
 			}
-			if (adjustedSize < threadsPerBlock) {
-				threadsPerBlock = adjustedSize;
-				if (threadsPerBlock % 32 != 0) {
-					threadsPerBlock += 32 - threadsPerBlock % 32;
-				}
-			}
-			numThreadBlocks = (adjustedSize + threadsPerBlock - 1) / threadsPerBlock;
-			//printf("Adjusted size:%d\tThreadBlocks:%d\tThreadsPerBlock:%d\n", adjustedSize, numThreadBlocks,threadsPerBlock);
 			// Launch a kernel on the GPU with one thread for each pair of elements.
-			sumKernel << <numThreadBlocks, threadsPerBlock,0, myStream >> > (gpuArray, adjustedSize);
+			binaryReduction << <blockCount, blockSize, 0, myStream >> > (gpuArray, adjustedSize);
 
 			// Check for any errors launching the kernel
 			cudaStatus = cudaGetLastError();
@@ -154,20 +152,9 @@ cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlo
 				fprintf(stderr, "sumArray Kernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
 				break;
 			}
-			else {
-				// cudaDeviceSynchronize waits for the kernel to finish, and returns
-				// any errors encountered during the launch.
-				cudaStreamSynchronize(myStream);
-				if (cudaStatus != cudaSuccess) {
-					fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-					break;
-				}
-				cudaMemcpyAsync(out, gpuArray, sizeof(long long), cudaMemcpyDeviceToHost,myStream);
-			}
 #ifdef TIMING
-			cudaEventRecord(endStep,myStream);
-			cudaStreamSynchronize(myStream);
-			//cudaStreamWaitEvent(myStream,endStep,0);
+			cudaEventRecord(endStep, myStream);
+			cudaStatus = cudaStreamSynchronize(myStream);
 			cudaEventElapsedTime(&gpuTimeUsed, startStep, endStep);
 			totalGpuTimeUsed += gpuTimeUsed;
 #endif // TIMING
@@ -175,8 +162,17 @@ cudaError_t sumArray(long long *arr, int size, long long *out, int threadsPerBlo
 #ifdef TIMING
 		avgGPUTime += totalGpuTimeUsed;
 #endif // TIMING
+		if (cudaStatus == cudaSuccess) {
+			cudaMemcpyAsync(out, gpuArray, sizeof(Type), cudaMemcpyDeviceToHost, myStream);
+			cudaStatus = cudaStreamSynchronize(myStream);
+			if (cudaStatus != cudaSuccess) {
+				fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
+			}
+		}
 		cudaStreamDestroy(myStream);
-		cudaFree(gpuArray);
+		if (arrayOnGPU) {
+			cudaFree(gpuArray);
+		}
 	}
 	return cudaStatus;
 }
