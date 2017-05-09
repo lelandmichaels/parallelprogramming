4ba53b4d81cc8cd9521e5cc826119ffc53a9ce19
Dalton <dnickers@unca.edu>
2017-05-08 21:29:45 -0400

    It runs. It's still not correct.

--- VisualStudioProjects/ArraySum/ArraySum/kernel.cu
+++ VisualStudioProjects/ArraySum/ArraySum/kernel.cu
@@ -2,19 +2,26 @@
 #include "device_launch_parameters.h"
 #include <stdio.h>
 
-cudaError_t sumArray(int *arr, unsigned long long size, long long *out, int threadsPerBlock);
+cudaError_t sumArray(int *arr, int size, int *out, int threadsPerBlock);
 
-__global__ void sumKernel(int *inArr, int *outArr)
+__global__ void sumKernel(int *arr, int size)
 {
-	int i = threadIdx.x * 2;
-	outArr[threadIdx.x] = inArr[i] + inArr[i + 1];
+	int i = threadIdx.x + blockDim.x*blockIdx.x;
+	if (i < size) {
+		arr[i] += arr[i+size];
+	}
 }
 
 int main()
 {
-	const int arraySize = 100;
-	int a[arraySize] = { 1, 2, 3, 4, 5 };
-	long long sum = 0;
+	const int arraySize = 20;
+	int a[arraySize];// = { 1, 2, 3, 4, 5,15 };
+	int sum = 0;
+	for (int i = 0; i < arraySize; i++) {
+		a[i] = i;
+		sum += a[i];
+	}
+	printf("Actual sum: %d\n", sum);
 	// Add vectors in parallel.
 	cudaError_t cudaStatus = sumArray(a, arraySize, &sum, 128);
 	if (cudaStatus != cudaSuccess) {
@@ -34,17 +41,8 @@ int main()
 }
 
 
-cudaError_t sumArray(int *arr, unsigned long long size, long long *out, int threadsPerBlock) {
-	unsigned long long adjustedSize = size;
-	if (adjustedSize % 32 != 0) {
-		adjustedSize += 32 - (size % 32);
-	}
-	if (adjustedSize < threadsPerBlock) {
-		threadsPerBlock = adjustedSize;
-	}
-	if (threadsPerBlock % 32 != 0) {
-		threadsPerBlock = threadsPerBlock + 32 - (threadsPerBlock % 32);
-	}
+cudaError_t sumArray(int *arr, int size, int *out, int threadsPerBlock) {
+
 	printf("Running array sum with %d threads per block.\n", threadsPerBlock);
 	cudaError_t cudaStatus;
 	// Choose which GPU to run on, change this on a multi-GPU system.
@@ -53,20 +51,26 @@ cudaError_t sumArray(int *arr, unsigned long long size, long long *out, int thre
 		fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
 	}
 	else {
+		int adjustedSize = size;
+		if (adjustedSize % 32 != 0) {
+			adjustedSize += 32 - (size % 32);
+		}
+		//determine number of thread blocks required
+		int numThreadBlocks;
 		//create array for sum reduction on gpu
-		int *gpuArray, gpuTempArray;
+		int *gpuArray;
 		cudaMalloc((void **)&gpuArray, adjustedSize * sizeof(int));
-		cudaMalloc((void **)&gpuTempArray, adjustedSize / 2 * sizeof(int));
 		//initialize array to 0 on gpu
 		cudaMemset((void*)gpuArray, 0, adjustedSize);
-		cudaMemset((void*)gpuTempArray, 0, adjustedSize / 2);
 		//copy input data to gpu
 		cudaMemcpy(gpuArray, arr, size, cudaMemcpyHostToDevice);
 		//keep reducing the problem size by two
 		while (adjustedSize > 1) {
+			adjustedSize /= 2;
+			numThreadBlocks = (adjustedSize + threadsPerBlock - 1) / threadsPerBlock;
+			printf("Adjusted size:%d\tThreadBlocks:%d\n", adjustedSize, numThreadBlocks);
 			// Launch a kernel on the GPU with one thread for each pair of elements.
-			sumKernel << <1, size >> > (gpuArray, gpuTempArray);
-
+			sumKernel << <numThreadBlocks, threadsPerBlock >> > (gpuArray, adjustedSize);
 
 			// Check for any errors launching the kernel
 			cudaStatus = cudaGetLastError();
@@ -78,13 +82,15 @@ cudaError_t sumArray(int *arr, unsigned long long size, long long *out, int thre
 				// cudaDeviceSynchronize waits for the kernel to finish, and returns
 				// any errors encountered during the launch.
 				cudaStatus = cudaDeviceSynchronize();
-				adjustedSize /= 2;
 				if (cudaStatus != cudaSuccess) {
 					fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
 					break;
 				}
+				cudaMemcpy(out, gpuArray, sizeof(int), cudaMemcpyDeviceToHost);
+				printf("PartialResult:%d\n", *out);
 			}
 		}
+		cudaFree(gpuArray);
 	}
 	return cudaStatus;
 }
