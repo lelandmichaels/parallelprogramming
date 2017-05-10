c7bb72f5f16b5075c8203db14a563a399dc712d2
johnlelandm <lmichae2@unca.edu>
2017-05-10 06:54:45 -0400

    Done with this one (from a while ago)

--- monteCarlo2/monteCarlo2/kernel.cu
+++ monteCarlo2/monteCarlo2/kernel.cu
@@ -6,61 +6,32 @@
 #include <curand.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <omp.h>
+#include <string.h>
 
 #define TIMING
-<<<<<<< HEAD
-#define N 100000
-=======
->>>>>>> origin/master
 
-/*__global__ void monteCarlo(unsigned  int n, unsigned int* inCirc_d, curandState_t* states, unsigned int seed)
+
+__global__ void monteCarlo(long long n, int* inCirc_d, curandState_t* states, unsigned int seed)
 {
 	unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-	if (index < n) {
-
-		double x, y;
-
-		curand_init(seed, index, 0, &states[index]);
-		//printf("%d\n", index);
+	int stride = blockDim.x*gridDim.x;
+	double x, y;
+	long long myTotal = 0;
+	curand_init(seed+index, 0/*index*/, 0, &states[index]);
+	for (long long i = index; i < n; i += stride) {
 		x = curand_uniform_double(&states[index]);
 		y = curand_uniform_double(&states[index]);
-
-		//printf("%.4f %.4f\n", x, y);
-
-		if ((x*x + y*y) <= 1.0) inCirc_d[index] = 1;
-		else inCirc_d = 0;
-
+		myTotal += ((x*x + y*y) <= 1.0);
 	}
-}*/
-
-__global__ void monteCarlo(unsigned  int n, unsigned int* inCirc_d, curandState_t* states, unsigned int seed)
-{
-	unsigned int index = blockIdx.x * blockDim.x + threadIdx.x;
-	unsigned int stride = blockDim.x*gridDim.x;
-	
-
-	for(unsigned int i = index; i < n; i += stride) {
-
-		double x, y;
-
-		curand_init(seed, i, 0, &states[i]);
-		//printf("%d\n", index);
-		x = curand_uniform_double(&states[i]);
-		y = curand_uniform_double(&states[i]);
-
-		//printf("%.4f %.4f\n", x, y);
-
-		if ((x*x + y*y) <= 1.0) inCirc_d[index] = 1;
-		else inCirc_d = 0;
-
-	}
+	inCirc_d[index] += myTotal;
 }
 
 //Used to check if there are any errors launching the kernel
 void CUDAErrorCheck()
-{	
-	
+{
+
 	cudaError_t error = cudaGetLastError();
 	if (error != cudaSuccess)
 	{
@@ -74,50 +45,74 @@ int main()
 	/*
 	creates cuda rand states and mallocs space for them, each state is thread safe
 	*/
-	
-	int N = 1000000;
-	int blockSize = 512;
-	int numBlocks = (N + blockSize - 1) / blockSize;
-	bool onGpu = false;
-
-	curandState_t* states;
-	cudaMalloc((void**)&states, N * sizeof(curandState_t));
-
-	unsigned int total = 0;
-	unsigned int* inCirc_d;
-	unsigned int* inCirc = (unsigned int*)malloc(N * sizeof(unsigned int));
-
-
-	cudaMalloc((void**)&inCirc_d, N * sizeof(unsigned int));
-	cudaMemset(inCirc_d, 0, N * sizeof(unsigned int));
-	//CUDAErrorCheck();
-	double cpu_estimate;
-
-	monteCarlo << <numBlocks, blockSize >> > (N, inCirc_d, states, time(0));
-
-	cudaDeviceSynchronize();
-	//CUDAErrorCheck();
-
-	cudaMemcpy(inCirc, inCirc_d, N * sizeof(int), cudaMemcpyDeviceToHost);
-	//CUDAErrorCheck();
-	cudaDeviceSynchronize();
-
-	//sumArray(inCirc, N, &total, numBlocks, blockSize, onGpu);
-	
-	for (unsigned int i = 0; i < N; i++) {
-		total += inCirc[i];
-		//printf("i = %d and count = %d\n",i,inCirc[i]);
+	cudaSetDevice(0);
+	cudaEvent_t gpuStart, gpuEnd;
+	cudaEventCreate(&gpuStart);
+	cudaEventCreate(&gpuEnd);
+	printf("Size");
+	for (int blocks = 1; blocks <= 128; blocks *= 2) {
+		printf("\t%d", blocks);
 	}
+	printf("\n");
+	for (long long tosses = 25000; tosses <= 10000000; tosses += 25000) {
+		printf("%d", tosses);
+		for (int blocks = 1; blocks <= 128; blocks *= 2) {
+			long long N = tosses;
+			int blockSize = 32;
+			int numBlocks = blocks;// (int)(N + blockSize - 1) / blockSize;
+
+			curandState_t* states;
+			cudaMalloc((void**)&states, blockSize * numBlocks * sizeof(curandState_t));
+
+			long long total = 0;
+			int* inCirc_d;
+			int* inCirc = (int*)malloc(blockSize * numBlocks * sizeof(int));
 
 
-	double pi_estimate = 4 * total / (double)N;
+			cudaMalloc((void**)&inCirc_d, blockSize * numBlocks * sizeof(int));
+			cudaMemset(inCirc_d, 0, blockSize * numBlocks * sizeof(int));
+			//CUDAErrorCheck();
 
-	printf("Total in Circle = %d\nEstimate of Pi = %.4f\n", total, pi_estimate);
+			cudaEventRecord(gpuStart);
+			cudaEventSynchronize(gpuStart);
+			monteCarlo << <numBlocks, blockSize >> > (N, inCirc_d, states, time(NULL));
+			cudaEventRecord(gpuEnd);
+			cudaEventSynchronize(gpuEnd);
+			cudaDeviceSynchronize();
 
-	cudaFree(states);
-	cudaFree(inCirc_d);
+			float msUsed;
+			cudaEventElapsedTime(&msUsed, gpuStart, gpuEnd);
+			//printf("%d	%lld	%f\n", blocks, tosses, msUsed);
+			printf("\t%f", msUsed);
 
 
+			//CUDAErrorCheck();
+
+			cudaMemcpy(inCirc, inCirc_d, blockSize * numBlocks * sizeof(int), cudaMemcpyDeviceToHost);
+			//CUDAErrorCheck();
+			cudaDeviceSynchronize();
+
+			//sumArray(inCirc, N, &total, numBlocks, blockSize, onGpu);
+
+			/*for (long long i = 0; i < blockSize * numBlocks; i++) {
+				total += inCirc[i];
+				//printf("i = %lld and count = %d\n", i, inCirc[i]);
+			}*/
+
+
+			//double pi_estimate = 4 * total / (double)N;
+
+			//printf("Total in Circle = %lld\nEstimate of Pi = %lf\n", total, pi_estimate);
+
+			cudaFree(states);
+			cudaFree(inCirc_d);
+		}
+		printf("\n");
+	}
+	cudaEventDestroy(gpuStart);
+	cudaEventDestroy(gpuEnd);
+
+	cudaDeviceReset();
 	return 0;
 }
 
